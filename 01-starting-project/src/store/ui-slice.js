dc71a693ee7fc1c125b89da6519603d2207c2586
Kavery pm <shiny.kavery@gmail.com>
2021-10-12 08:16:22 -0300

    feat(uiSlice): added a ui slice

---
+++ 01-starting-project/src/store/ui-slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartIsvisible: true },
+  reducers: {
+    toggle(state) {
+      state.cartIsvisible = !state.cartIsvisible;
+    },
+  },
+});
+export const uiActions = uiSlice.actions;
+export default uiSlice;
