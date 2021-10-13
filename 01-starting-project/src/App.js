a3fe336ac69aa020e3d79b200c35ba269610e0f0
Kavery pm <shiny.kavery@gmail.com>
2021-10-12 23:58:23 -0300

    added the cart data to the database

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -2,9 +2,20 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 function App() {
   const isCartVisible = useSelector((state) => state.ui.cartIsVisible);
-  console.log("hello" + isCartVisible);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+  useEffect(() => {
+    fetch('https://reacthook-f4714.firebaseio.com/cart.json',{
+      method:'PUT',
+      body:JSON.stringify(cart),
+
+    })
+   
+  }, [cart]);
+ 
   return (
     <Layout>
       {!isCartVisible && <Cart />}
